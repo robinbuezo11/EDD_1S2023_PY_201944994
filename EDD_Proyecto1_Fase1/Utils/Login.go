5475779b1023ed8f48d0c9736fdcbc7b20268a71
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2023-02-22 01:09:46 -0600

    [add] Menu admin

--- EDD_Proyecto1_Fase1/Utils/Login.go
+++ EDD_Proyecto1_Fase1/Utils/Login.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func Login() {
 
+	admin := User{firstname: "admin", pass: "admin"}
+
 	option := 0
 
 	for option != 2 {
@@ -23,6 +25,21 @@ func Login() {
 			pass := ""
 			fmt.Println("\nIngresa tu password: ")
 			fmt.Scan(&pass)
+
+			if user == admin.firstname && pass == admin.pass {
+				for option != 5 {
+					fmt.Println("*** Dashboard Administrador - EDD GoDrive ***")
+					fmt.Println("*      1. Ver estudiantes Pendientes        *")
+					fmt.Println("*      2. Ver estudiantes del Sistema       *")
+					fmt.Println("*      3. Registrar Nuevo Estudiante        *")
+					fmt.Println("*      4. Carga Masiva de Estudiantes       *")
+					fmt.Println("*      5. Cerrar Sesion                     *")
+					fmt.Println("*********************************************")
+					fmt.Println("\nElige una opcion: ")
+
+					fmt.Scan(&option)
+				}
+			}
 		}
 	}
 
